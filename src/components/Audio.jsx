c7957e875ee7ab620616c4d631a613848c79f636
Vũ Đức An <98570451+da0ran9e@users.noreply.github.com>
2026-06-18 13:22:43 +0700

    Add files via upload

--- src/components/Audio.jsx
+++ src/components/Audio.jsx
@@ -1,6 +1,9 @@
 import { PositionalAudio } from "@react-three/drei"
 import { useStore } from "../hooks/useStore"
 
+const WAVES_URL = `${import.meta.env.BASE_URL}sounds/waves.mp3`
+const BIRDS_URL = `${import.meta.env.BASE_URL}sounds/birds.mp3`
+
 export const Audio = () => {
   const audioEnabled = useStore((state) => state.audioEnabled)
 
@@ -8,21 +11,11 @@ export const Audio = () => {
     audioEnabled && (
       <>
         <group position={[0, 0, 0]}>
-          <PositionalAudio
-            autoplay
-            loop
-            url="/sounds/waves.mp3"
-            distance={50}
-          />
+          <PositionalAudio autoplay loop url={WAVES_URL} distance={50} />
         </group>
 
         <group position={[-65, 35, -55]}>
-          <PositionalAudio
-            autoplay
-            loop
-            url="/sounds/birds.mp3"
-            distance={30}
-          />
+          <PositionalAudio autoplay loop url={BIRDS_URL} distance={30} />
         </group>
       </>
     )
